f5835db0ba1fefb40f252bf6af12422098e3dc48
Samuel Silva <samuelsljr@gmail.com>
2024-05-04 19:46:43 -0300

    navbar and mobile

--- Mentor/assets/js/main.js
+++ Mentor/assets/js/main.js
@@ -76,6 +76,17 @@
     }
   }, true)
 
+/*
+    Nav Links Actived for each Click
+*/
+const activePage = window.location.pathname;
+const navLinks = document.querySelectorAll('.nav-link').
+forEach(link => {
+  if(link.href.includes(`${activePage}`)){
+    link.classList.add('active');
+  }
+})
+
   /**
    * Preloader
    */
